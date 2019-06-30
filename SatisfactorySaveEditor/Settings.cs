82d5b5fe6aac6ec97c2e89b4be58cb6ac7dc3740
Kevin Gut <kevin.gut@bitm.sg>
2019-06-30 13:23:48 +0200

    Add Settings class

---
+++ SatisfactorySaveEditor/Settings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SatisfactorySaveEditor
+{
+    [Serializable]
+    public class Settings
+    {
+        public bool ShowResizeHint
+        { get; set; }
+        public bool ShowLimited
+        { get; set; }
+        public bool ShowDuplicationHint
+        { get; set; }
+        public bool ShowDeletionHint
+        { get; set; }
+
+        public Settings()
+        {
+            ShowResizeHint =
+                ShowLimited =
+                ShowDuplicationHint =
+                ShowDeletionHint =
+                true;
+        }
+
+        public static Settings Load(string Content)
+        {
+            var S = new XmlSerializer(typeof(Settings));
+            using (var SR = new StringReader(Content))
+            {
+                return (Settings)S.Deserialize(SR);
+            }
+        }
+
+        public string Save()
+        {
+            var S = new XmlSerializer(typeof(Settings));
+            using (var SW = new StringWriter())
+            {
+                S.Serialize(SW, this);
+                return SW.ToString();
+            }
+        }
+    }
+}
